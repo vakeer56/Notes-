0194613d91ab8e558fe10250235a890c4c75f5eb
vakeer56 <mvvvhomes@gmail.com>
2025-11-02 09:32:35 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const port = 5000;
+
+//establishing connection b/w app and database
+mongoose.connect("mongodb://localhost:27017/notesDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Connected to MongoDB successfully");
+});
+db.on("error", (err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uplaods')) //to access the files publicly
+
+app.get("/", (req, res) => {
+    res.json({ message: "The server is working fine", status: 200 });
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+});

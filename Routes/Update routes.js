ff1e9ae4c654fb444441c9a518c8bad8a1d051a0
vakeer56 <mvvvhomes@gmail.com>
2025-11-11 12:40:05 +0530

    sample commit

--- Routes/Update routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const notesController = require("../controller/notesController");
-
-// Update note status (approve/reject)
-router.patch("/updateStatus", notesController.updateStatus);
-
-module.exports = router;

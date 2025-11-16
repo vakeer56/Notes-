2dd50046de30d0c58e808fab1e4c06ce2ff4a632
vakeer56 <mvvvhomes@gmail.com>
2025-11-17 05:10:46 +0530

    added again

--- Routes/notesRoutes.js
+++ Routes/notesRoutes.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Note = require('../model/notes');
 const { store } = require('../Controllers/notesController');
+const { index } = require('../Controllers/notesController');
+const { show } = require('../Controllers/notesController');
+const { byDepartment } = require('../Controllers/notesController');
+const { bySubject } = require('../Controllers/notesController');
+const { byYear } = require('../Controllers/notesController');
 
-router.post('/notes/upload', store);
+
+
+router.post('/upload', store);
+router.get('/allnotes', index);
+router.get('/student/:studentID', show);
+router.get('/department/:department', byDepartment);
+router.get('/subject/:title', bySubject)
+router.get('/year/:year', byYear);
+
 
 
 

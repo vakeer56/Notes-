d315149b0a236e08c1da28e8603e6d69d89b0ef5
PR <ponnurajan.1314@gmail.com>
2025-12-31 17:07:02 +0530

    GET all notes in home page

---
+++ frontend/src/components/NoteCard.jsx
@@ -0,0 +1,24 @@
+
+
+function NoteCard({ note }){
+    return(
+        <>
+        <div className="note-card">
+            <h3 className="note-title">{note.title}</h3>
+
+            <p className="note-desc">
+                {note.description}
+            </p>
+
+            <div className="note-meta">
+                <span>Department: {note.department}</span><br/>
+                <span>Year: {note.year}</span>
+            </div>
+
+            <a href={note.file_url} className="view-btn">View Notes</a>
+        </div>
+        </>
+    );
+}
+
+export default NoteCard
